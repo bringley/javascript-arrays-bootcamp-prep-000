5a57409fb8cef01c6c28860259ad1792e42c88da
Brian Ringley <brian.ringley@wework.com>
2018-01-27 15:48:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -57,4 +57,13 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   return array.unshift(element)
 }
 
+function addElementToEndOfArray(array, element) {
+  var newArray = [...array, element]
+  return newArray
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  return array.push(element)
+}
+
 
