59366cd44fa99cabaea275bc4d70c2f8fb8798e8
Brian Ringley <brian.ringley@wework.com>
2018-01-27 16:05:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -92,3 +92,8 @@ function removeElementFromBeginningOfArray(array) {
 function destructivelyRemoveElementFromEndOfArray(array) {
   return array.pop()
 }
+
+function removeElementFromEndOfArray(array) {
+  var newArray = array.slice(0, array.length - 1)
+  return newArray
+}
