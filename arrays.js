e5e12d6d06ddd291998cd71f72a908d6559bfbd7
Brian Ringley <brian.ringley@wework.com>
2018-01-27 15:37:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,49 @@
-var ingredient1 = ''
+var ingredient1 = 'bread'
+var ingredient2 = 'mild cheese'
+var ingredient3 = 'sharp cheese'
+var ingredient4 = 'butter'
+var ingredient5 = 'tomato'
+var ingredient6 = 'garlic'
+
+//array literals list values within square brackets
+var grilledCheeseIngredients = [
+  'bread', 
+  'mild cheese', 
+  'sharp cheese', 
+  'butter', 
+  'tomato', 
+  'garlic'
+]
+
+var tomatoSauceIngredients = [
+  'tomato',
+  'garlic',
+  'olive oil',
+  'basil',
+  'oregano'
+]
+
+//you can also use the array constructor
+var evenNumbers = new Array()
+
+//todo
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+]
+
+//there are methods for mutating arrays
+//push() adds item to end of array
+//unshift() adds item to start of array
+
+//the spread operator allows for a new array in place rather than mutating the original array
+var numbers = [1, 2, 3]
+//[0, ...numbers]
+//[...numbers, 4]
+//you can also mutate the original array if it's a variable
+numbers = [0, ...numbers]
+numbers = [numbers..., 4]
+
+
